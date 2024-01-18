24a8522d43079ae714d0975dc05a198c07634b7a
Sintry1 <Dominic_smith2012@hotmail.co.uk>
2024-01-18 14:59:14 +0100

    Removed .env from cache

--- WhistleblowerSolution.Server/obj/Debug/net8.0/WhistleblowerSolution.Server.AssemblyInfo.cs
+++ WhistleblowerSolution.Server/obj/Debug/net8.0/WhistleblowerSolution.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhistleblowerSolution.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaec262f127114328612734bc5def5b97365ed74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af379e2fc1757cfaca0e9c49c29f7ed099b3e0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhistleblowerSolution.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhistleblowerSolution.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
